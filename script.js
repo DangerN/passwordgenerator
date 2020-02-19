6ea5dcf1a90a3ce8857b8ada54404af5947ccdaa
Josh Everett <josheverett93@gmail.com>
2020-02-18 21:38:15 -0600

    added initial prompt about password length

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var length = prompt("Choose your password length. It must be least 8 characters and no more than 128 characters");
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
