a5e35cb9845e4c4e6832bafa9adbd435c4078f88
Josh Everett <josheverett93@gmail.com>
2020-02-19 19:50:28 -0600

    edited readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # passwordgenerator
+
+Live Link: https://jeverett93.github.io/passwordgenerator/
+
+Contributors: jeverett93.github.io
+
+Technology Stack: HTML5, CSS3, Javascript
+
+Contact: 
+josheverett93@gmail.com
+https://www.linkedin.com/in/joshua-everett-087a4649/
+https://github.com/jeverett93
+
+About: This is an application that generates a random password based on user-selected criteria which includes: password length between 8-128 characters, an options to include uppercase letters, lowercase letters, numbers, and special characters.
--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Prompts for password criteria
+var length = prompt("Choose your password length. It must be least 8 characters and no more than 128 characters");
+var lowerCase = prompt("Do you want lowercase characters?");
+var upperCase = prompt("Do you want uppercase characters?");
+var numeber = prompt("Do you want Numbers?");
+var special = prompt("Do you want special characters?");
+
 // Write password to the #password input
 function writePassword() {
-  var length = prompt("Choose your password length. It must be least 8 characters and no more than 128 characters");
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
