c0a785c539e026bff16094888fc41929b8523e5c
Josh Everett <josheverett93@gmail.com>
2020-02-20 17:35:23 -0600

    created values for password criteria

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+// Pseudo Code
+// Click generate password button and see prompts for password criteria.
+// First ask for password length between 8-128 characters
+// Validate user selection
+// Then ask if they want lowercase letters and validate user selection.
+// Then ask if they want uppercase letters and validate user selection.
+// Then ask if they want numbers and validate user selection.
+// Then ask if they want special characters and validate user selection.
+// Once user had made all their selections, a password generates either in an alert or written on the page using all the criteria the user selected.
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Prompts for password criteria
-var length = prompt("Choose your password length. It must be least 8 characters and no more than 128 characters");
-var lowerCase = prompt("Do you want lowercase characters?");
-var upperCase = prompt("Do you want uppercase characters?");
-var numeber = prompt("Do you want Numbers?");
-var special = prompt("Do you want special characters?");
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -18,6 +25,42 @@ function writePassword() {
 
 
 }
+
+// Generating password
+function generatePassword() {
+  var length = prompt("Choose your password length. It must be least 8 characters and no more than 128 characters");
+    var lowerCase = confirm("Do you want lowercase letters?");
+  var upperCase = confirm("Do you want uppercase letters?");
+  var numbers = confirm("Do you want Numbers?");
+  var special = confirm("Do you want special characters?");
+
+  // Password length based on user prompt input
+if (length >=8 && length <=128) {
+  var lengthValue = parseInt(lengthCount);
+}
+else {
+  alert("Invalid value. Please try again.")
+}
+// Upper/Lower case values based on user confirm input
+if (upperCase === true) {
+upperCaseValue = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+}
+else {
+upperCaseValue = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+}
+// Numbers values based on user confirm input
+if (numbers === true) {
+numbersValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+}
+else {
+numbersValue = void(0);
+}
+// Special Characters values based on user confirm input
+if (special === true) {
+specialValue = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "'", "<", ">", "?", "/", "`", "~"];
+
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
